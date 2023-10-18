85199d2622769acd2bfc4ff1b0a1c85e32117b6d
y367855 <takumi367855@gmail.com>
2023-10-18 19:04:41 +0900

    test

---
+++ src/main/java/com/java/yaala/bean/YaalaHand.java
@@ -0,0 +1,38 @@
+package com.java.yaala.bean;
+
+public enum YaalaHand {
+    NOHANDS("ブタ", 0),
+    ONEPAIR("ワンペア", 50),
+    TWOPAIR("ツーペア", 100),
+    THREECARD("スリーカード", 150),
+    FOURCARD("フォーカード", 200),
+    FULLHOUSE("フルハウス", 200),
+    STRAIGHT("ストレート", 250),
+    FLASH("フラッシュ", 250),
+    STRAIGHTFLASH("ストレートフラッシュ", 350),
+    SUNSHINE("サンシャイン", 300),
+    TWINSUN("ツインサン", 250),
+    STARFLASH("スターフラッシュ", 400),
+    DIAHOUSE("ダイアハウス", 350),
+    SPADELEADER("スペード・リーダー", 250),
+    SPADEDIAMIX("スペード・ダイアミックス", 350),
+    MARKWONDER("マークワンダー", 300),
+    SUNARCH("サンアーチ", 9999);
+    
+    
+    final private String name;
+    final private int point;
+    
+    YaalaHand(String name, int point){
+        this.name = name;
+        this.point = point;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getPoint() {
+        return point;
+    }
+}
